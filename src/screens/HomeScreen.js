8faf3530d4a1d6ff75dee77171aac1d1ea659545
gabriel-paulino <gabriel.paulino104@gmail.com>
2021-10-02 15:40:52 -0300

    Using TouchableOpacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 
 const HomeScreen = ( {navigation, route} ) => {
@@ -7,30 +7,42 @@ const HomeScreen = ( {navigation, route} ) => {
     return(
         <View style={styles.mainView}>
             <Text>Home Screen</Text>
-            <Button
-            title="Star Wars"
+            <TouchableOpacity
+            style={styles.touchableView}
             onPress={ () => {
                 navigation.navigate("HomeToDetails", {movie: {
                     title: "Star Wars",
                     release: 1997,
                     screenNumber: 1
-                }} )}}/>
-            <Button
-            title="Black Phanter"
+                }} )}}>
+                    <View>
+                        <Text style={styles.touchableText}>Star Wars</Text>
+                    </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.touchableView}            
             onPress={ () => {
                 navigation.navigate("HomeToDetails", {movie: {
                     title: "Black Phanter",
                     release: 2018,
                     screenNumber: 1
-                }} )}}/>
-            <Button
-            title="The Matrix"
+                }} )}}>
+                    <View>
+                        <Text style={styles.touchableText}>Black Phanter</Text>
+                    </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.touchableView}
             onPress={ () => {
                 navigation.navigate("HomeToDetails", {movie: {
                     title: "The Matrix",
                     release: 1999,
                     screenNumber: 1
-                }} )}}/>
+                }} )}}>
+                    <View>
+                        <Text style={styles.touchableText}>The Matrix</Text>
+                    </View>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -40,7 +52,23 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-    }
+    },
+    touchableView: {
+        marginBottom: 30,
+        width: 150,
+        height: 50,
+        alignItems: 'center',
+        backgroundColor: 'orange',
+        borderWidth: 5,
+        borderRadius: 10,
+    },
+    touchableText: {
+        textAlign: 'center',
+        paddingTop: 5,
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white'
+    },
 })
 
 export default HomeScreen;
